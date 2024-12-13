898a0d29013968ad57d9eb7fcda99e64d111830b
Illia Khristinchenko <ikhristi@c1r1s2.42wolfsburg.de>
2024-12-13 12:09:53 +0100

    Fixed ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,15 +1,20 @@
 #include "BitcoinExchange.hpp"
  
-int main()
+int main(int argc, char **argv)
 {
     BitcoinExchange btc;
+    if(argc != 2)
+    {
+        std::cerr << "You must have 2 params" << std::endl;
+        return 1;
+    } 
     if (!btc.loadExchangeRates("data.csv"))
     {
         std::cerr << "Cannot read database" << std::endl;
         return 1;
     }
-
-    std::ifstream inputFile("input.txt");
+    
+    std::ifstream inputFile(argv[1]);
     if (!inputFile.is_open())
     {
         std::cerr << "Error: could not open file." << std::endl;
@@ -54,7 +59,7 @@ int main()
             continue;
         }
         float rate = btc.getExchangeRate(date);
-        std::cout << date << " => " << value << " = " << std::fixed << std::setprecision(2) << value * rate << std::endl;
+        std::cout << date << " => " << value << " = " << std::fixed << std::setprecision(2) << value * rate << std::endl << std::setprecision(0);
     }
     inputFile.close();
     return 0;
